003244d10875cadfa076225e21e9b3a748fc56a7
PatrykPorabik <patryk@porabik.pl>
2020-06-19 21:26:40 +0200

    git push

--- js/app.js
+++ js/app.js
@@ -104,7 +104,6 @@ document.addEventListener('DOMContentLoaded', () => {
             displayShape()
             addScore()
             gameOver()
-            newGame()
         }
     }
 
@@ -207,4 +206,10 @@ document.addEventListener('DOMContentLoaded', () => {
             clearInterval(timerId)
         }
     }
+    window.addEventListener("keydown", function(e) {
+        // space and arrow keys
+        if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
 })
